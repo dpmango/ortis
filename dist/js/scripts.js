bee5b210dc7fc0f952355c6c19a614a21e3c84a3
Kirill <kirillpwnography@gmail.com>
2017-09-03 00:54:18 +0300

    intermediate commit

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
 	// Верхний слайдер
 	$('.topslider').slick({
 		slidesToScroll: 1,
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		arrows: false,
 		dots: true
 	});
-	
+
 	// Популярные товары
 	$('.pop-goods__slider').slick({
 		slidesToScroll: 1,
@@ -15,7 +15,7 @@ $(document).ready(function(){
 		arrows: true,
 		dots: false
 	});
-	
+
 	// Отзывы
 	$('.reviews__slider').slick({
 		slidesToScroll: 1,
@@ -23,5 +23,32 @@ $(document).ready(function(){
 		arrows: true,
 		dots: false
 	});
-	
+
+	// Прилипающее подменю
+	$(window).scroll(function () {
+		if ($(window).width() > '1439') {
+			var topPos = $('.submenu-helper').offset().top - $(window).scrollTop();
+			var currentHeight = $('.submenu').innerHeight();
+			if (topPos <= 48) {
+				$('.submenu').addClass('sticky');
+				$('.submenu-helper').height(currentHeight);
+			} else {
+				$('.submenu').removeClass('sticky');
+				$('.submenu-helper').height(1);
+			}
+		}
+	});
+
+	// Логотип при скролле
+	$(window).scroll(function () {
+		if ($(window).width() > '1439') {
+			var topPos = $('.topbar__logo').offset().top - $(window).scrollTop();
+			if (topPos <= 38) {
+				$('.page-header__logo').addClass('active');
+			} else {
+				$('.page-header__logo').removeClass('active');
+			}
+		}
+	});
+
 });
