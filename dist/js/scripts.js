9679b7a92d57bb7424d9f1b79b962db685109efc
Kirill <kirillpwnography@gmail.com>
2017-09-03 15:56:23 +0300

    intermediate commit

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -62,14 +62,14 @@ $(document).ready(function () {
 
 	// Прилипающее подменю
 	$(window).scroll(function () {
-		if ($(window).width() > '1439') {
+		if ($(window).width() > '1023') {
 			var topPos = $('.submenu-helper').offset().top - $(window).scrollTop();
-			var currentHeight = $('.submenu').innerHeight();
+			var currentHeight = $('.menu-wrap').innerHeight();
 			if (topPos <= 48) {
-				$('.submenu').addClass('sticky');
+				$('.menu-wrap').addClass('sticky');
 				$('.submenu-helper').height(currentHeight);
 			} else {
-				$('.submenu').removeClass('sticky');
+				$('.menu-wrap').removeClass('sticky');
 				$('.submenu-helper').height(1);
 			}
 		}
@@ -87,4 +87,75 @@ $(document).ready(function () {
 		}
 	});
 
+	// Телефон при скролле
+	$(window).scroll(function () {
+		if ($(window).width() > '1439') {
+			var topPos = $('.topbar__info').offset().top - $(window).scrollTop();
+			if (topPos <= 38) {
+				$('.page-header__tel').addClass('active');
+				$('.page-header__input').addClass('hidden');
+			} else {
+				$('.page-header__tel').removeClass('active');
+				$('.page-header__input').removeClass('hidden');
+			}
+		}
+	});
+
+	// Поиск по клику
+	$('.page-header__btn').on('click', function (e) {
+		if ($('.page-header__input').hasClass('hidden')) {
+			e.preventDefault();
+			$('.page-header__input').removeClass('hidden');
+			$('.page-header__tel').removeClass('active');
+		} else {
+			$('.page-header__btn').unbind('submit').submit();
+		}
+		if ($(window).width() < '1440') {
+			if ($('.page-header__input').hasClass('mobile-active')) {
+				$('.page-header__input').removeClass('mobile-active');
+				$('.page-header__btn').unbind('submit').submit();
+			} else {
+				$('.page-header__input').addClass('mobile-active');
+				e.preventDefault();
+			}
+		}
+	})
+	$(document).mouseup(function (a) {
+		var form = $('.page-header__input');
+		if (a.target != form[0] && !form.has(a.target).length) {
+			form.removeClass('mobile-active');
+		}
+	});
+
+	// Подменю
+	$('.submenu__link').on('click', function (e) {
+		e.preventDefault();
+		var dataDrop = $(this).attr('data-drop');
+		$('.submenu__link').removeClass('active');
+		$(this).addClass('active');
+
+		$('.popup-menu').slideDown(200);
+		$('.popup-menu__wrap').removeClass('active');
+		$('.popup-menu__wrap#' + dataDrop).addClass('active');
+	});
+
+	$('.popup-menu__link').on('click', function (e) {
+		e.preventDefault();
+		var dataDrop = $(this).attr('data-cat');
+		$(this).parents('.popup-menu__half').children('.popup-menu__link').removeClass('active');
+		$(this).addClass('active');
+
+		$(this).parents('.popup-menu__wrap').find('.popup-menu__item').removeClass('active');
+		$('.popup-menu__item#' + dataDrop).addClass('active');
+	});
+
+	$(document).mouseup(function (a) {
+		var menu = $('.popup-menu');
+		var links = $('.submenu');
+
+		if (a.target != menu[0] && !menu.has(a.target).length && a.target != links[0] && !links.has(a.target).length) {
+			menu.slideUp(200);
+			$('.submenu__link').removeClass('active');
+		}
+	});
 });
